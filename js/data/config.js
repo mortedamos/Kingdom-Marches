224e39b96f088a695c6d624abcd547b5bf994e72
snicolen <snicolen@gmail.com>
2026-08-20 21:03:22 -0400

    cleanup

--- js/data/config.js
+++ js/data/config.js
@@ -65,9 +65,9 @@ window.GameConfig = {
     /** Local date this build was cut, YYYY-MM-DD. */
     date: "2026-08-20",
     /** Local time this build was cut, 24-hour HH:MM. */
-    time: "21:02",
+    time: "21:03",
     /** Monotonic build counter -- increment it, don't recompute it. */
-    number: 160,
+    number: 161,
   },
 
   // =========================================================================
