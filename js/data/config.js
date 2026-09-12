0c0d0978154f9d3fc64e4eebc248d8c6c6f0062d
snicolen <snicolen@gmail.com>
2026-09-11 20:06:37 -0400

    Sound FX!

--- js/data/config.js
+++ js/data/config.js
@@ -65,9 +65,9 @@ window.GameConfig = {
     /** Local date this build was cut, YYYY-MM-DD. */
     date: "2026-09-11",
     /** Local time this build was cut, 24-hour HH:MM. */
-    time: "16:18",
+    time: "20:06",
     /** Monotonic build counter -- increment it, don't recompute it. */
-    number: 288,
+    number: 289,
   },
 
   // =========================================================================
